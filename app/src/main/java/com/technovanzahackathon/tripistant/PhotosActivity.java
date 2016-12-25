044dc41d5a4606e5744b4277ce1ed1372d9670dc
Saeed Jassani <saeedjassani@gmail.com>
2016-12-25 12:06:19 +0530

    Final Commit

--- app/src/main/java/com/technovanzahackathon/tripistant/PhotosActivity.java
+++ app/src/main/java/com/technovanzahackathon/tripistant/PhotosActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -28,26 +27,16 @@ public class PhotosActivity extends AppCompatActivity implements View.OnClickLis
 
 	@Override
 	public void onClick(View view) {
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, getImageUri());
-		startActivityForResult(intent, TAKE_PHOTO_CODE);
+		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+		startActivity(intent);
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 
-		if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
-			Uri imagePath = getImageUri();
-			File dir = new File (String.valueOf(imagePath));
-			dir.mkdirs();
-		}
 	}
 
-	/**
-	 * Get the uri of the captured file
-	 * @return A Uri which path is the path of an image file, stored on the dcim folder
-	 */
 	private Uri getImageUri() {
 		// Store image in dcim
 		File file = new File(Environment.getExternalStorageDirectory() + "/TripIstant", CAPTURE_TITLE);
